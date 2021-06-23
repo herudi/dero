4ee96fced0848d92b48619c0f85f506a5e27bbe0
herudi <herudi7@gmail.com>
2021-06-23 13:56:43 +0700

    add v1

--- examples/with_crud_mysql/deps.ts
+++ examples/with_crud_mysql/deps.ts
@@ -11,9 +11,9 @@ export {
     Controller,
     Dero,
     Status
-} from "./../../mod.ts";
+} from "https://deno.land/x/dero@1.0.0/mod.ts";
 
-export { IsString, IsNumber } from './../../validator.ts';
+export { IsString, IsNumber } from 'https://deno.land/x/dero@1.0.0/validator.ts';
 
 // mysql client
 export { Client } from "https://deno.land/x/mysql@v2.8.0/mod.ts";
